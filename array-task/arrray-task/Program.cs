6b1f8f5042d006ac5d94d6c185aa3838cf5088cc
Sabit <ceferovsabit999@gmail.com>
2024-10-20 00:29:16 +0400

    added tasks

---
+++ array-task/arrray-task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace arrray_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int[] numbers = { 8, 5, 10, 14 };
+            //int max = numbers[0];
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //        max = numbers[i];
+            //}
+            //Console.WriteLine($"{max} en boyuk ededdir.");
+            #endregion
+
+            #region Task2
+
+            //var product1 = new { Id = 1, Name = "Lays", Price = 3, StockCount = 150 };
+            //var product2 = new { Id = 2, Name = "Chipsim", Price = 2, StockCount = 300 };
+            //var product3 = new { Id = 3, Name = "Kurasan", Price = 1 , StockCount = 250 };
+            //var product4 = new { Id = 4, Name = "Pringles", Price = 5, StockCount = 75 };
+
+            //var products = new[] { product1, product2, product3, product4 };
+            //int average = 0;
+            //int sum = 0;
+            //int count = 0;
+
+            //for (int i = 0; i < products.Length; i++) 
+            //{
+            //    if (products[i].Id % 2 != 0) 
+            //    {
+            //        sum += products[i].Price;
+            //        count++;
+            //    }                                
+            //}
+            //average = sum / count;
+            //Console.WriteLine($"Average of price: {average} ");
+            #endregion
+        }
+    }
+}
